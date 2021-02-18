f06e9b1657f99b8e5d479e098c36815a6dd8900b
DominiqueMakowski <dom.mak19@gmail.com>
2021-02-18 11:00:48 +0800

    Update fixation_cross.js

--- jspsych_extensions/fixation_cross.js
+++ jspsych_extensions/fixation_cross.js
@@ -3,5 +3,5 @@ var fixation_cross = {
     stimulus: "<div style='font-size:5em'>+</div>",
     choices: jsPsych.NO_KEYS,
     trial_duration: function () { return randomInteger(250, 750) },
-    data: { object: 'fixation_cross' }
+    data: { screen: 'fixation_cross' }
 }
