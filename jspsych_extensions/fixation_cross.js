08af479f7c0cf6846adc1568404c417ed4763179
DominiqueMakowski <dom.mak19@gmail.com>
2021-02-18 11:01:25 +0800

    Delete fixation_cross.js

--- jspsych_extensions/fixation_cross.js
+++
@@ -1,7 +0,0 @@
-var fixation_cross = {
-    type: 'html-keyboard-response',
-    stimulus: "<div style='font-size:5em'>+</div>",
-    choices: jsPsych.NO_KEYS,
-    trial_duration: function () { return randomInteger(250, 750) },
-    data: { screen: 'fixation_cross' }
-}
