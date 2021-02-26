4dfaf85d27af1748cea7d105cc077807c743857e
Zen <zen-juen@users.noreply.github.com>
2021-02-26 21:44:36 +0800

    add cumulative_probability

--- misc/utils.js
+++ misc/utils.js
@@ -30,6 +30,25 @@ function isString(x) {
     return typeof x === 'string' || x instanceof String
 }
 
+function cumulative_probability(x, mean, sd) {
+    var z = (x - mean) / Math.sqrt(2 * sd * sd)
+    var t = 1 / (1 + 0.3275911 * Math.abs(z))
+    var a1 = 0.254829592
+    var a2 = -0.284496736
+    var a3 = 1.421413741
+    var a4 = -1.453152027
+    var a5 = 1.061405429
+    var erf = 1 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-z * z)
+    var sign = 1
+    if (z < 0) {
+        sign = -1
+    }
+    return (1 / 2) * (1 + sign * erf)
+}
+// obtained from https://stackoverflow.com/questions/5259421/cumulative-distribution-function-in-javascript
+// supported by https://stackoverflow.com/questions/457408/is-there-an-easily-available-implementation-of-erf-for-python
+
+
 // function saveData(name, data) {
 //     return JSON.stringify({ filename: name, filedata: data })
 // }
