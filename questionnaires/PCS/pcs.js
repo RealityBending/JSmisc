a6b5cf33b6e7960e9e526ad0ca96dff2befe5b88
Dominique Makowski <dom.mak19@gmail.com>
2024-04-10 16:17:18 +0100

    add more parts

--- questionnaires/PCS/pcs.js
+++ questionnaires/PCS/pcs.js
@@ -1,7 +1,15 @@
 // Put code here
 var pcs_preload = {
     type: jsPsychPreload,
-    audio: ["audio/PC1.mp3", "audio/PC2.mp3", "audio/PC3.mp3", "audio/PC4.mp3"],
+    audio: [
+        "audio/PC1a.mp3",
+        "audio/PC1b.mp3",
+        "audio/PC1c.mp3",
+        "audio/PC2a.mp3",
+        "audio/PC2b.mp3",
+        "audio/PC3.mp3",
+        "audio/PC4.mp3",
+    ],
     images: ["stimulus.png"],
 }
 
@@ -16,33 +24,78 @@ var pcs_instructions = {
     choices: ["Start"],
 }
 
-var pcs_part1 = {
+// Part 1 ========================================================================
+var pcs_part1a = {
     type: jsPsychAudioKeyboardResponse,
     on_start: function () {
         document.body.style.cursor = "none"
     },
-    stimulus: ["audio/PC1.mp3"],
+    stimulus: ["audio/PC1a.mp3"],
     prompt: "<img src='headphones.png'>",
     choices: ["s"],
     response_ends_trial: true,
     trial_ends_after_audio: true,
 }
 
-var pcs_part2 = {
+var pcs_part1b = {
     type: jsPsychAudioKeyboardResponse,
-    stimulus: ["audio/PC2.mp3"],
+    stimulus: ["audio/PC1b.mp3"],
     prompt: "<img src='stimulus.png'>",
     choices: ["s"],
     response_ends_trial: true,
     trial_ends_after_audio: true,
 }
 
+var pcs_part1c = {
+    type: jsPsychAudioKeyboardResponse,
+    prompt: '<p style="color:white;">Please wait...</p>',
+    stimulus: ["audio/PC1c.mp3"],
+    choices: ["s"],
+    response_ends_trial: true,
+    trial_ends_after_audio: true,
+}
+
+var pcs_part1_q1 = {
+    type: jsPsychSurveyText,
+    questions: [{ prompt: '<p style="color:white;">Color: </p>', name: "PCS_Q1", required: true }],
+}
+
+// Part 2 ========================================================================
+var pcs_part1a = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: ["audio/PC2a.mp3"],
+    choices: ["s"],
+    response_ends_trial: true,
+    trial_ends_after_audio: true,
+}
+
+// Evaluatiopn ========================================================================
+var pcs_assessment = {
+    type: jsPsychSurveyLikert,
+    on_start: function () {
+        document.body.style.backgroundColor = "white"
+        document.body.style.cursor = "auto"
+    },
+    preamble: "<h2>Few questions...</h2>",
+    questions: [
+        {
+            prompt: "How deep was your experience?",
+            name: "PCS_Depth",
+            labels: ["Not at all", "1", "2", "3", "4", "Very deep"],
+        },
+        {
+            prompt: "How strongly did you feel your hand become heavy, BLABLA...",
+            name: "PCS_HandLowering",
+            labels: ["Normal heaviness", "1", "2", "3", "4", "Very heavy"],
+        },
+    ],
+}
 var pcs_finish = {
     type: jsPsychHtmlButtonResponse,
     on_start: function () {
         document.body.style.backgroundColor = "white"
         document.body.style.cursor = "auto"
     },
-    stimulus: "<h1>End</h1>" + "<p>Thank for completing this part of the experiment</p>",
+    stimulus: "<h1>End</h1>" + "<p>Thank for completing this part of the experiment. BLABLA</p>",
     choices: ["Continue"],
 }
